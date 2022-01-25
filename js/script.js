f8295f460e18af8e654f4a4641330a49035304e6
Artem <example@gmail.com>
2022-01-26 00:38:53 +0500

    Урок № 17

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?'
-
+/* 
 2) Создать объект personalMovieDB и в него поместить такие свойства:
     - count - сюда передается ответ на первый вопрос
     - movies - в это свойство поместить пустой объект
@@ -21,24 +21,128 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const str = "test"
+//const arr = [1, 2, 4]; 
 
-const personalMovieDB = {
+//console.log(str[2] = 'd');
+//console.log(str.toUpperCase());
+console.log(str.toLowerCase()); 
+console.log(str); 
+
+const fruit = "Some fruit"
+
+console.log(fruit.indexOf("fruit")); 
+const logg = "Hello World"; 
+
+//console.log(logg.slice(6));
+
+//console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6,5));
+
+const num = 12.2; 
+console.log(Math.round(num)); 
+
+const test = "12.2px"; 
+//console.log(parseInt(test));
+console.log(parseFloat(test));
+
+/* 
+let num = 20;
+
+function showFirstMessage(text){
+    console.log(text);
+    let num = 10;
+    console.log(num);
+}
+
+showFirstMessage("Hello World!"); 
+console.log(num);  */
+
+/* function calc(a, b) {
+    return (a + b); 
+}
+
+console.log(calc(4, 3)); 
+console.log(calc(5, 6)); 
+console.log(calc(10, 6));
+ */
+/* function ret() {
+    let num = 50; 
+    return num; 
+}
+
+const anotherNum = ret(); 
+console.log(anotherNum); 
+
+const logger = function () {
+    console.log("Hello");
+};
+
+logger();
+
+const calc = (a, b) => { return a + b };  */
+
+
+/* const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+/* 
+//if (numberOfFilms === []) {
+    console.log('Error'); 
+} else if (num < 50) {
+    console.log('Много');
+} else {
+    console.log('Ok!'); 
+} */
+
+/* const personalMovieDB = {
     count: numberOfFilms,
     movies: {}, 
     actors: {},
     genres: [], 
     privat: false
-}; 
+};  */
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', ''); 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+/* for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+          
+    personalMovieDB.movies[a] = b;
+} */
 
-console.log(personalMovieDB); 
+/* if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман"); 
+} else {
+    console.log("Произошла ошибка"); 
+}
+
+console.log(personalMovieDB);  */ 
+
